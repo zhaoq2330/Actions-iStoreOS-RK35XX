ab3ef6ead590f3b3f76a40d7dbbc066acbec81a6
50687505@qq.com <50687505@qq.com>
2025-04-19 13:48:02 +0800

    add mt7615e driver

--- diy-part2.sh
+++ diy-part2.sh
@@ -95,8 +95,10 @@ chmod 755 package/base-files/files/etc/init.d/swconfig_install
 
 # 集成 nsy_g68-plus WiFi驱动
 mkdir -p package/base-files/files/lib/firmware/mediatek
-cp -f $GITHUB_WORKSPACE/configfiles/mt7915_eeprom.bin package/base-files/files/lib/firmware/mediatek/mt7915_eeprom.bin
-cp -f $GITHUB_WORKSPACE/configfiles/mt7916_eeprom.bin package/base-files/files/lib/firmware/mediatek/mt7916_eeprom.bin
+cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7615_cr4.bin package/base-files/files/lib/firmware/mediatek/mt7615_cr4.bin
+cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7615_n9.bin package/base-files/files/lib/firmware/mediatek/mt7615_n9.bin
+cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7615_rom_patch.bin package/base-files/files/lib/firmware/mediatek/mt7615_rom_patch.bin
+cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7916_eeprom.bin package/base-files/files/lib/firmware/mediatek/mt7916_eeprom.bin
 
 
 
